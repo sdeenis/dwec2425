8a2ab49f2df8142ea53fad99a57354d194a2cfd3
sdeenis <sdenis2011@hotmail.es>
2025-01-24 13:46:33 +0100

    fin clase

--- Apuntes/ejerciciosClase/202.js
+++ Apuntes/ejerciciosClase/202.js
@@ -3,15 +3,26 @@ window.onload = iniciar;
 
 function iniciar() {
     document.getElementById("form").addEventListener('submit', validar, false);
+    console.log("cargado")
 }
 
 function validarNIF() {
     let NIF = document.getElementById("NIF");
     let NIFv = NIF.value;
 
-    if (NIFv[0] != )
+    if (!isNaN(NIFv[0])) {
+        alert("El primer carácter ha de ser una letra");
+        return false;
+    }
+
+    return true;
 }
 
-function validar() {
-
+function validar(e) {
+    console.log("comienzo")
+    if (validarNIF() === true) {
+        return true;
+    } 
+    e.preventDefault();
+    return false;
 }
