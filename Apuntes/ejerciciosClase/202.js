92615da60e1b22f621bab62a58ed4a4c5feec9a4
sdeenis <sdenis2011@hotmail.es>
2025-01-24 13:32:30 +0100

    bhkgk

---
+++ Apuntes/ejerciciosClase/202.js
@@ -0,0 +1,17 @@
+
+window.onload = iniciar;
+
+function iniciar() {
+    document.getElementById("form").addEventListener('submit', validar, false);
+}
+
+function validarNIF() {
+    let NIF = document.getElementById("NIF");
+    let NIFv = NIF.value;
+
+    if (NIFv[0] != )
+}
+
+function validar() {
+
+}
