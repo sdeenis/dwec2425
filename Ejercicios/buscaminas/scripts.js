35516012ad9c6cb2165643d431aaf62ac76c049a
sdeenis <sdenis2011@hotmail.es>
2025-02-25 01:02:40 +0100

    buscaminas finalizado

--- Ejercicios/buscaminas/scripts.js
+++ Ejercicios/buscaminas/scripts.js
@@ -1,9 +1,74 @@
 //variables globales
-let numeroFilas, numeroColumnas, numeroBombas, cronometro 
-let numeroCasillasDesmarcadas = 0
+let numeroFilas, numeroColumnas, numeroBombas, cronometro
+let numeroCasillasDesmarcadas
+let numeroBanderas
+
+//comprobamos la sesion nada mas cargar la pagina
+window.onload = comprobarSesion
+
+function comprobarSesion() {
+    console.log("comprobando sesion")
+    if (localStorage.getItem("contenidoBody")) {
+        console.log("sesion iniciada")
+        console.log(localStorage.getItem("contenidoBody"))
+
+        document.body.innerHTML = localStorage.getItem("contenidoBody")
+
+        //hemos de asignar los listeners de nuevo pq no se pasan al localStorage
+        asignarEventListeners();
+
+        //recuperamos todo
+        arrayBombas = localStorage.getItem("arrayBombas")
+        numeroCasillasDesmarcadas = document.getElementsByClassName("casillaabierta").length
+        numeroFilas = localStorage.getItem("numeroFilas")
+        numeroColumnas = localStorage.getItem("numeroColumnas")
+        numeroBombas = localStorage.getItem("numeroBombas")
+        numeroBanderas = localStorage.getItem("numeroBanderas")
+        document.getElementsByClassName("numBanderas")[0].textContent = `${numeroBanderas} 🚩`
+        
+        //recuperamos el cronometro
+        let segundosGuardados = Number(localStorage.getItem("segundosTranscurridos"));
+        let minutosGuardados = Number(localStorage.getItem("minutosTranscurridos"));
+
+        empezarCronometro(minutosGuardados, segundosGuardados);
+    } else {
+        console.log("sesion no iniciada")
+    }
+}
+
+function finalizarSesion() {
+    localStorage.removeItem("contenidoBody");
+    localStorage.removeItem("arrayBombas");
+    localStorage.removeItem("numeroFilas");
+    localStorage.removeItem("numeroColumnas");
+    localStorage.removeItem("numeroBombas");
+    localStorage.removeItem("numeroBanderas");
+    localStorage.removeItem("segundosTranscurridos");
+    localStorage.removeItem("minutosTranscurridos");
+}
+
+function guardarSesion() {
+    localStorage.setItem("contenidoBody", document.body.innerHTML)
+    localStorage.setItem("arrayBombas", arrayBombas)
+    localStorage.setItem("numeroFilas", numeroFilas)
+    localStorage.setItem("numeroColumnas", numeroColumnas)
+    localStorage.setItem("numeroBombas", numeroBombas)
+    localStorage.setItem("numeroBanderas", numeroBanderas)
+}
+
+function asignarEventListeners() {
+    let casillas = document.getElementsByClassName("casilla");
+    for (let casilla of casillas) {
+        if (!casilla.classList.contains("casillaabierta")) {
+            casilla.addEventListener("click", abrirCasilla)
+            casilla.addEventListener("contextmenu", marcarCasilla)
+        }
+    }
+}
 
 
 function iniciarJuego() {
+    finalizarSesion()
     numeroCasillasDesmarcadas = 0
     let dificultad = document.getElementById("dificultad").value
     console.log(`dificultad seleccionada: ${dificultad}`)
@@ -13,21 +78,30 @@ function iniciarJuego() {
     empezarCronometro()
 }
 
-function empezarCronometro() {
+function empezarCronometro(minutosInic = 0, segundosInic = 0) {
     clearInterval(cronometro)
-    document.getElementsByClassName("cronometro")[0].textContent = "00:00 ⏱"
-    let segundos = 0
-    let minutos = 0
+
+    let segundos = segundosInic
+    let minutos = minutosInic
+
+    let minStr = minutos.toString().padStart(2, '0')
+    let segStr = segundos.toString().padStart(2, '0')
+
+    document.getElementsByClassName("cronometro")[0].textContent = `${minStr}:${segStr} ⏱`
+
     cronometro = setInterval(() => {
         segundos++
         if (segundos === 60) {
             minutos++
             segundos = 0
         }
-        let minStr = minutos.toString().padStart(2, '0') 
-        let segStr = segundos.toString().padStart(2, '0') 
+        let minStr = minutos.toString().padStart(2, '0')
+        let segStr = segundos.toString().padStart(2, '0')
 
         document.getElementsByClassName("cronometro")[0].textContent = `${minStr}:${segStr} ⏱`
+
+        localStorage.setItem("segundosTranscurridos", segundos);
+        localStorage.setItem("minutosTranscurridos", minutos);
     }, 1000)
 }
 
@@ -41,22 +115,26 @@ function asignarVariables(dificultad) {
         case "facil":
             numeroFilas = 10
             numeroColumnas = 10
-            numeroBombas = 1
+            numeroBombas = 3
+            numeroBanderas = 3
             break
         case "media":
             numeroFilas = 18
             numeroColumnas = 18
             numeroBombas = 97
+            numeroBanderas = 97
             break
         case "dificil":
             numeroFilas = 24
             numeroColumnas = 24
             numeroBombas = 172
+            numeroBanderas = 172
             break
     }
 }
 
 function generarTablero() {
+    //en caso de empezar una partida nueva, hemos de borrar los <p> de victoria o derrota
     document.getElementById("mensajeDerrota").style.display = "none"
     document.getElementById("mensajeVictoria").style.display = "none"
 
@@ -68,7 +146,7 @@ function generarTablero() {
 
     let numBanderas = document.createElement("div")
     numBanderas.className = "numBanderas"
-    numBanderas.textContent = `${numeroBombas} 🚩`
+    numBanderas.textContent = `${numeroBanderas} 🚩`
 
     let cronometro = document.createElement("div")
     cronometro.className = "cronometro"
@@ -78,14 +156,14 @@ function generarTablero() {
     filaSuperior.appendChild(cronometro)
 
     tablero.appendChild(filaSuperior)
-    
+
     for (let i = 0; i < numeroFilas; i++) {
         let fila = document.createElement("div")
         fila.className = "fila"
         for (let j = 0; j < numeroColumnas; j++) {
             let casilla = document.createElement("div")
             casilla.className = "casilla"
-            casilla.id = `${i}-${j}`
+            casilla.id = `${i}-${j}` //le damos ID dinamico
             casilla.addEventListener("click", abrirCasilla)
             casilla.addEventListener("contextmenu", marcarCasilla)
             fila.appendChild(casilla)
@@ -104,6 +182,7 @@ function generarBombas() {
 
         bombas.add(coorXY); // solo se añadira si no está repetido
     }
+    localStorage.setItem("arrayBombas", Array.from(bombas))
     return Array.from(bombas); //devolvemos el set convertido en array
 }
 
@@ -122,26 +201,41 @@ function abrirCasilla(e) {
             e.target.textContent = cont
             e.target.removeEventListener("click", abrirCasilla)
             e.target.removeEventListener("contextmenu", marcarCasilla)
+            
             numeroCasillasDesmarcadas++
             comprobarVictoria()
             console.log(`casillas desmarcadas ${numeroCasillasDesmarcadas}`)
+
         } else {
             abrirVecinas(e.target.id)
             e.target.classList.add("casillaabierta");
             e.target.removeEventListener("click", abrirCasilla)
             e.target.removeEventListener("contextmenu", marcarCasilla)
-            
+
             console.log(`casillas desmarcadas ${numeroCasillasDesmarcadas}`)
+
             comprobarVictoria()
         }
     }
 }
 
 function comprobarVictoria() {
+    console.log(`estoy comprobando victoria ${numeroCasillasDesmarcadas} ?= ${((numeroFilas * numeroColumnas) - numeroBombas)}`)
     if (numeroCasillasDesmarcadas == ((numeroFilas * numeroColumnas) - numeroBombas)) {
+
+        let casillas = document.getElementsByClassName("casilla")
+        for (let casilla of casillas) {
+            casilla.removeEventListener("click", abrirCasilla)
+            casilla.removeEventListener("contextmenu", marcarCasilla)
+        }
+
         let mensajeVictoria = document.getElementById("mensajeVictoria");
         mensajeVictoria.textContent = "¡Has ganado! 🎉";
-        mensajeVictoria.style.display = "block"; 
+        mensajeVictoria.style.display = "block";
+        pararCronometro()
+        finalizarSesion()
+    } else {
+        guardarSesion()
     }
 }
 
@@ -155,10 +249,11 @@ function hasPerdido() {
         }
     }
     pararCronometro()
+    finalizarSesion()
 
     let mensajeDerrota = document.getElementById("mensajeDerrota");
     mensajeDerrota.textContent = "¡Has perdido! 😞";
-    mensajeDerrota.style.display = "block"; 
+    mensajeDerrota.style.display = "block";
 }
 
 function abrirVecinas(posicion) {
@@ -176,10 +271,10 @@ function abrirVecinas(posicion) {
                 casilla.removeEventListener("click", abrirCasilla)
                 casilla.removeEventListener("contextmenu", marcarCasilla)
 
-                if (cont === 0) { 
+                if (cont === 0) {
                     casilla.innerHTML = ""
                     numeroCasillasDesmarcadas++
-                    abrirVecinas(nuevaPosicion) 
+                    abrirVecinas(nuevaPosicion)
                 } else {
                     casilla.classList.add(`casilla-${cont}`)
                     casilla.innerHTML = cont
@@ -218,9 +313,11 @@ function marcarCasilla(e) {
 
     if (e.target.classList.contains("marcada")) {
         e.target.classList.remove("marcada");
-        document.getElementsByClassName("numBanderas")[0].textContent = `${++numeroBombas} 🚩`;
-    } else { 
+        document.getElementsByClassName("numBanderas")[0].textContent = `${++numeroBanderas} 🚩`;
+        guardarSesion()
+    } else {
         e.target.classList.add("marcada");
-        document.getElementsByClassName("numBanderas")[0].textContent = `${--numeroBombas} 🚩`;
+        document.getElementsByClassName("numBanderas")[0].textContent = `${--numeroBanderas} 🚩`;
+        guardarSesion()
     }
 }
